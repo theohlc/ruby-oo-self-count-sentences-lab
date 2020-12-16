09dbbf655f6d2a7f9174a5801189b9c1b4c1e81a
theo <teddyhoover@gmail.com>
2020-12-16 13:00:41 -0600

    commit tree da15c816ae2cc4df8f574b23827a09d8084cccb9

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences_array = self.split(/[.?!]/)
+    sentences_array.reject! { |c| c.empty?}
+    sentences_array.count
   end
 end
